8ca4a64d1cf9cb0aa980050309c2d476620dbe0b
13aba <bayarsaikhan.bayarbat@gmail.com>
2023-12-05 19:26:11 +0800

    -Added admin user
    -Created super user admin and registered the models so we can create new data entry in the database from admin site

--- midterm/actAir/admin.py
+++ midterm/actAir/admin.py
@@ -2,3 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 
+#Everything below here are my own code
+
+from .models import * 
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('location_name', 'gps')
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('CO2', 'O3', 'NO2', 'PM10', 'PM2_5', 'DateTime', 'location')
+
+
+admin.site.register(Location, LocationAdmin)
+
+admin.site.register(Measurement, MeasurementAdmin)
